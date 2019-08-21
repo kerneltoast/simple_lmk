dadd79701363cf4d9c78f89527ee0a7d99812469
Sultan Alsawaf <sultan@kerneltoast.com>
2019-08-21 08:30:55 -0700

    simple_lmk: Remove kthread_should_stop() exit condition
    Simple LMK's reclaim thread should never stop; there's no need to have
    this check.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -250,13 +250,7 @@ static int simple_lmk_reclaim_thread(void *data)
 	sched_setscheduler_nocheck(current, SCHED_FIFO, &sched_max_rt_prio);
 
 	while (1) {
-		bool should_stop;
-
-		wait_event(oom_waitq, (should_stop = kthread_should_stop()) ||
-				      READ_ONCE(needs_reclaim));
-
-		if (should_stop)
-			break;
+		wait_event(oom_waitq, READ_ONCE(needs_reclaim));
 
 		/*
 		 * Kill a batch of processes and wait for their memory to be
