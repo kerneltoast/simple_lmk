56ed93a8286ace0a7b900a56d9a41ddf35e37812
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-13 23:17:41 -0800

    mm: vmpressure: Don't export tunables to userspace
    Userspace could change these tunables and make Simple LMK function
    poorly. Don't export them.
    
    Reported-by: attack11 <fernandobouchet@gmail.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -37,13 +37,9 @@ static const unsigned int vmpressure_level_med = 60;
 static const unsigned int vmpressure_level_critical = 95;
 
 static unsigned long vmpressure_scale_max = 100;
-module_param_named(vmpressure_scale_max, vmpressure_scale_max,
-			ulong, S_IRUGO | S_IWUSR);
 
 /* vmpressure values >= this will be scaled based on allocstalls */
 static unsigned long allocstall_threshold = 70;
-module_param_named(allocstall_threshold, allocstall_threshold,
-			ulong, S_IRUGO | S_IWUSR);
 
 static struct vmpressure global_vmpressure;
 static BLOCKING_NOTIFIER_HEAD(vmpressure_notifier);
