5d3f7c78fbad4a7f0bc4ea1316b47f72ea80fb09
Sultan Alsawaf <sultan@kerneltoast.com>
2021-04-26 16:03:56 -0700

    simple_lmk: Don't block in simple_lmk_mm_freed() on mm_free_lock
    When the mm_free_lock write lock is held, it means that reclaim is
    either starting or ending, in which case there's nothing that needs to
    be done in simple_lmk_mm_freed(). We can use a trylock here instead to
    avoid blocking.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2019-2020 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2019-2021 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 
 #define pr_fmt(fmt) "simple_lmk: " fmt
@@ -266,7 +266,10 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 {
 	int i;
 
-	read_lock(&mm_free_lock);
+	/* Nothing to do when reclaim is starting or ending */
+	if (!read_trylock(&mm_free_lock))
+		return;
+
 	for (i = 0; i < nr_victims; i++) {
 		if (victims[i].mm == mm) {
 			victims[i].mm = NULL;
