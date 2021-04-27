2ef33e53c7b0763a18831af3f5e6e47d1492d652
Sultan Alsawaf <sultan@kerneltoast.com>
2021-04-26 17:53:14 -0700

    simple_lmk: Optimize victim finder to eliminate hard-coded adj ranges
    Hard-coding adj ranges to search for victims results in a few problems.
    Firstly, the hard-coded adjs must be vigilantly updated to match what
    userspace uses, which makes long-term support a headache. Secondly, a
    full traversal of every running process must be done for each adj range,
    which can turn out to be quite expensive, especially if userspace
    assigns many different adj values and we want to enumerate them all.
    This leads us to the final problem, which is that processes with
    different adjs within the same hard-coded adj range will be treated the
    same, even though they're not: the process with a higher adj is less
    important, and the process with a lower adj is more important. This
    could be fixed by enumerating every possible adj, but again, that would
    necessitate several scans through the active process list, which is bad
    for performance, especially since latency is critical here.
    
    Since adjs are only 16 bits, and we only care about positive adjs, that
    leaves us with 15 bits of the adj that matter. This is a relatively
    small number of potential adjs (32,768), which makes it possible to
    allocate a static array that's indexed using the adj. Each entry in this
    array is a pointer to the first task_struct in a singly-linked list of
    task_structs sharing an adj. A `simple_lmk_next` member is added to
    task_struct to accommodate this linked list. The victim finder now
    iterates downward through the array searching for linked lists of tasks,
    starting from the highest adj found, so that the lowest-priority
    processes are always considered first for reclaim. This fixes all of the
    problems mentioned above, and now there is only one traversal through
    every running process. The array itself only takes up 256 KiB of memory
    on 64-bit, which is a very small price to pay for the advantages gained.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1260,6 +1260,9 @@ struct task_struct {
 	/* Used by LSM modules for access restriction: */
 	void				*security;
 #endif
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
+	struct task_struct		*simple_lmk_next;
+#endif
 
 #ifdef CONFIG_GCC_PLUGIN_STACKLEAK
 	unsigned long			lowest_stack;
