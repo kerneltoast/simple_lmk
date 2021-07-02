44627f99d851d8dcf199fb47199dbc6883aa3b4a
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-01 23:07:00 -0700

    mm: vmpressure: Ignore costly-order allocations for direct reclaim too
    The direct reclaim vmpressure path was erroneously excluded from the
    PAGE_ALLOC_COSTLY_ORDER check which was added in commit "mm: vmpressure:
    Ignore allocation orders above PAGE_ALLOC_COSTLY_ORDER".
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3031,7 +3031,7 @@ retry:
 
 	do {
 		vmpressure_prio(sc->gfp_mask, sc->target_mem_cgroup,
-				sc->priority);
+				sc->priority, sc->order);
 		sc->nr_scanned = 0;
 		shrink_zones(zonelist, sc);
 
